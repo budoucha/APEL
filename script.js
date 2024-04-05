8f8fd87a2cb87c567cf84d923985a822524773f5
budoucha <budoucha@gmail.com>
2024-04-05 19:50:41 +0900

    attribute id to cell

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 
 document.addEventListener('DOMContentLoaded', () => {
+  let buttonNum = 0;
   const addBtnEl = document.querySelector('button');
   addBtnEl.addEventListener('click', () => {
     const templateContent = document.querySelector('template#cell-template').content;
     const clone = document.importNode(templateContent, true);
+    clone.querySelector("div.cell").id = `cell-${buttonNum}`;
     const btnContainer = document.querySelector('div.button-container');
     document.querySelector('section#main').insertBefore(clone, btnContainer);
+    buttonNum++;
   });
   addBtnEl.click();
 });
