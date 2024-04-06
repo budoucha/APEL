ea431a4c470da78e62c10d1f6ff910465a9fa79d
budoucha <budoucha@gmail.com>
2024-04-07 02:01:08 +0900

    delete-cell button

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-  
+  const g = {
+    // global variables
+    cellNum: 0,
+  };
+
   // add-cell button
   const addBtnEl = document.querySelector('button.add-cell');
   addBtnEl.addEventListener('click', () => {
+    const currentCell = g.cellNum;
     const templateContent = document.querySelector('template#cell-template').content;
     const clone = document.importNode(templateContent, true);
-    clone.querySelector("div.cell").id = `cell-${buttonNum}`;
+    clone.querySelector("div.cell").id = `cell-${currentCell}`;
+
+    //delete-cell-button
+    const deleteBtnEl = clone.querySelector('button.delete-cell');
+    deleteBtnEl.addEventListener('click', () => {
+      const cellId = `div#cell-${currentCell}`;
+      const cellEl = document.querySelector(cellId);
+      cellEl.remove();
+    });
+    // add cell
     const btnContainer = document.querySelector('div.button-container');
     document.querySelector('section#main').insertBefore(clone, btnContainer);
-    buttonNum++;
+    g.cellNum++;
   });
   addBtnEl.click();
 });
