1500941f103d1ae12e4d436229424514283fcf3a
budoucha <budoucha@gmail.com>
2024-04-13 23:40:46 +0900

    updated showconfirm.js

--- showConfirm/showConfirm.js
+++ showConfirm/showConfirm.js
@@ -3,7 +3,7 @@ export default async function showConfirm(message, callbacks = []) {
 
   // set paths
   const moduleUrl = new URL(import.meta.url);
-  const basePath = new URL('.', moduleUrl).href; // モジュールのディレクトリ
+  const basePath = new URL('.', moduleUrl).href;
   const htmlPath = new URL('./showConfirm.html', basePath).href;
   const cssPath = new URL('./showConfirm.css', basePath).href;
 
@@ -39,7 +39,7 @@ export default async function showConfirm(message, callbacks = []) {
     });
   } else if (okCallback.function === null) {
     // don't show ok button if ok is overriden by null
-    okBtnEl.style.display = 'none';
+    okBtnEl.remove();
   } else {
     okBtnEl.textContent = okCallback.label ?? 'OK';
     okBtnEl.addEventListener('click', () => {
@@ -48,7 +48,7 @@ export default async function showConfirm(message, callbacks = []) {
     });
   }
 
-  //// others
+  //// other buttons
   const otherCallbacks = callbacks.filter(c => c.name !== "ok");
   otherCallbacks.forEach(callback => {
     const buttonEl = document.createElement('button');
@@ -60,6 +60,15 @@ export default async function showConfirm(message, callbacks = []) {
     alertEl.querySelector('div.buttons').appendChild(buttonEl);
   });
 
+  // if no button is created any click will close the alert
+  const buttonEls = alertEl.querySelectorAll('button');
+  if (buttonEls.length === 0) {
+    alertEl.querySelector('div.buttons').remove();
+    alertEl.addEventListener('click', () => {
+      console.log('click');
+      alertEl.remove();
+    });
+  }
 
   const alertContainer = document.querySelector('body');
   alertContainer.appendChild(alertEl);
